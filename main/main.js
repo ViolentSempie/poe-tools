50abfa833736c554cf156bfd8dd29a9917af7b87
ViolentSempie <private@sempie.dev>
2024-04-12 00:12:07 +0200

    feat: implemented as overlay

--- main/main.js
+++ main/main.js
@@ -1,42 +1,77 @@
-import { app, BrowserWindow } from "electron";
-import serve from "electron-serve";
-import path, { join } from "path";
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const serve = require("electron-serve");
+const path = require("path");
+const { OverlayController, OVERLAY_WINDOW_OPTS } = require("electron-overlay-window");
 
 const appServe = app.isPackaged ? serve({
-    directory: join(__dirname, "../out")
+    directory: path.join(__dirname, "../out")
 }) : null;
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+const toggleMouseKey = "CmdOrCtrl + J";
+let window = null;
+
+const createOverlay = () => {
+    window = new BrowserWindow({
+        width: 400,
+        height: 300,
         webPreferences: {
-            preload: join(__dirname, "preload.js")
-        }
+            preload: path.join(__dirname, "preload.js"),
+            nodeIntegration: true
+        },
+        ...OVERLAY_WINDOW_OPTS,
     });
 
     // If the app is build, we serve the build version
     if (app.isPackaged) {
-        appServe(win).then(() => {
-            win.loadURL("app://-");
+        appServe(window).then(() => {
+            window.loadURL("app://-");
         });
-
-        return;
+    } else {
+        // Load Next.JS app in development
+        window.loadURL("http://localhost:3000");
+        window.webContents.openDevTools({ mode: "detach", activate: false });
+        window.webContents.on("did-fail-load", (e, code, desc) => {
+            window.webContents.reloadIgnoringCache();
+        });
     }
 
-    // Load Next.JS app in development
-    win.loadURL("http://localhost:3000");
-    win.webContents.openDevTools();
-    win.webContents.on("did-fail-load", (e, code, desc) => {
-        win.webContents.reloadIgnoringCache();
+    makeDemoInteractive();
+
+    OverlayController.attachByTitle(
+        window,
+        "Path of Exile",
+        { hasTitleBarOnMac: true }
+    );
+};
+
+function makeDemoInteractive() {
+    let isInteractive = false;
+
+    function toggleOverlayState() {
+        if (isInteractive) {
+            isInteractive = false;
+            OverlayController.focusTarget();
+            window.webContents.send("focus-change", false);
+        } else {
+            isInteractive = true;
+            OverlayController.activateOverlay();
+            window.webContents.send("focus-change", true);
+        }
+    }
+
+    window.on("blur", () => {
+        isInteractive = false;
+        window.webContents.send("focus-change", false);
     });
+
+    globalShortcut.register(toggleMouseKey, toggleOverlayState);
 }
 
 app.on("ready", () => {
-    createWindow();
+    setTimeout(
+        createOverlay,
+        process.platform === "linux" ? 1000 : 0 // https://github.com/electron/electron/issues/16809
+    );
 });
 
 app.on("window-all-closed", () => {
---
+++ src/app/settings/settings.tsx
@@ -0,0 +1,12 @@
+export function Settings() {
+    return (
+        <div className="absolute bottom-[200px] left-[300px]">
+            <button
+                type="button"
+                className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 active:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+                Settings
+            </button>
+        </div>
+    );
+}
