5d8516e712a44ade24a1fbe3ea86542dfd3b03cf
ViolentSempie <private@sempie.dev>
2024-04-14 11:01:21 +0200

    fix: render waypoint correctly-ish

--- src/leveling-tracker/route-processing/fragments/waypoint-part.tsx
+++ src/leveling-tracker/route-processing/fragments/waypoint-part.tsx
@@ -1,6 +1,8 @@
 export function WaypointPart() {
     return (
         <div className="relative flex space-x-3">
+            <div className="flex items-center w-6 justify-center"></div>
+
             <div className="flex flex-row min-w-0 flex-1 space-x-1 pt-0.5">
                 <p className="font-medium text-blue-600">
                     waypoint
