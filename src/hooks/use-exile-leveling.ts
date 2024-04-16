40c0da8a7ee76f75a365591d9bae7c0dee815be1
ViolentSempie <private@sempie.dev>
2024-04-16 18:36:35 +0200

    feat: implemented poe-log-events

--- src/hooks/use-exile-leveling.ts
+++ src/hooks/use-exile-leveling.ts
@@ -1,34 +1,29 @@
+import { AreaEnteredEvent } from 'poe-log-events/dist/events/AreaEnteredEvent';
 import { Fragments } from "@/leveling-tracker/route-processing/fragments/types";
-import { ClientEvent, ClientEventType } from "@/poe-events/types/client-event";
-import { EnteredClientEvent } from "@/poe-events/types/entered-client-event";
 import { useLevelingStore } from "@/stores/leveling";
 import { getPart } from "@/utils/get-part";
 import { useEffect } from "react";
+import { getArea } from '@/utils/get-area';
 
 export function useExileLeveling() {
     const data = useLevelingStore(state => state.currentSteps);
     const setData = useLevelingStore(state => state.setCurrentSteps);
 
     useEffect(() => {
-        window.electron.on("poe-client-event", function (event, data: ClientEvent) {
-            // parse event
-            if (data.type !== ClientEventType.Entered) {
-                return;
-            }
-
+        window.electron.on("poe-client-area-entered", function (event, data: AreaEnteredEvent) {
             const { getSection, step, setStep, currentSteps } = useLevelingStore.getState();
             const currentSection = getSection();
 
             const enterStep = currentSteps[currentSteps.length - 1];
             const nextStep = step + currentSteps.length;
 
-            const enteredEvent = data as EnteredClientEvent;
             const enterStepLocation = getPart(enterStep, "enter") as Fragments.EnterFragment;
             const waypointStepLocation = getPart(enterStep, "waypoint_use") as Fragments.WaypointUseFragment;
             const logoutStepLocation = getPart(enterStep, "logout") as Fragments.LogoutFragment;
             const nextLocation = enterStepLocation?.areaId ?? waypointStepLocation?.dstAreaId ?? logoutStepLocation?.areaId ?? -1;
+            const area = getArea(nextLocation);
 
-            if (enteredEvent.locationId !== nextLocation) {
+            if (area.name !== data.newArea) {
                 return;
             }
 
--- src/poe-events/poe-event-manager.ts
+++ src/poe-events/poe-event-manager.ts
@@ -1,69 +1,35 @@
-import { Tail } from "tail";
 import { getGamePath } from "steam-game-path";
-import { parseEnteredEvent, parseGeneratingEvent } from "./types/entered-client-event";
-import { ClientEvent, ClientEventType } from "./types/client-event";
-import { ipcMain } from "electron";
+import { Language, LogOptions, PathOfExileLog } from "poe-log-events";
+import { AreaEnteredEvent } from "poe-log-events/dist/events/AreaEnteredEvent";
 
 // steam id 238960
 
-const ClientEventTypes: ((baseData: ClientEvent, extra: string[]) => ClientEvent | null)[] = [
-    parseGeneratingEvent,
-    parseEnteredEvent,
-];
-
 export class PoeEventManager {
-    private _tail: Tail | null = null;
+    private _poeLog: PathOfExileLog;
     private _mainWindow: Electron.CrossProcessExports.BrowserWindow;
 
     constructor(mainWindow: Electron.CrossProcessExports.BrowserWindow) {
         const gamePath = getGamePath(238960);
         const logFilePath = gamePath?.game?.path + "/logs/Client.txt";
 
-        this._tail = new Tail(logFilePath);
-        this._tail.on("line", (line: string) => this.on(line));
-        this._tail.on("error", (error: Error) => this.error(error));
+        const logOptions: LogOptions = {
+            logFilePath,
+            ignoreDebug: true,
+            language: Language.English,
+        };
+
+        this._poeLog = new PathOfExileLog(logOptions);
         this._mainWindow = mainWindow;
+
+        this._poeLog.on("areaEntered", this.onAreaChanged.bind(this));
+        this._poeLog.on("error", this.error.bind(this));
     }
 
-    destroy() {
-        this._tail?.unwatch();
+    onAreaChanged(event: AreaEnteredEvent) {
+        this._mainWindow.webContents.send("poe-client-area-entered", event);
     }
 
-    on(line: string) {
-        // 2024/04/12 22:09:07 7530873 cff94598 [INFO Client 288] : You have entered Luxurious Hideout.
-        // eslint-disable-next-line prefer-const
-        let [date, time, messageId, /* we dont need this field */, messageSeverity, messageSource, sourceId, separator, ...rest] = line.split(" ");
-
-        if (separator.trim() !== ":") {
-            rest = [separator, ...rest];
-            separator = ":";
-        }
-
-        messageSeverity = messageSeverity.substring(1);
-        sourceId = sourceId.substring(0, sourceId.length - 1);
-
-        const baseEvent: ClientEvent = {
-            id: messageId,
-            timestamp: (new Date(date + " " + time)).getTime(),
-            severity: messageSeverity,
-            sourceId: sourceId,
-            sourceType: messageSource,
-            type: ClientEventType.Unknown,
-        }
-
-        for (const eventType of ClientEventTypes) {
-            const fullEvent = eventType(baseEvent, rest);
-
-            if (fullEvent === null) {
-                continue;
-            }
-
-            console.log("[debug] firing off event", fullEvent);
-            this._mainWindow.webContents.send("poe-client-event", fullEvent);
-        }
-    }
-
-    error(error: Error) {
+    error(error: unknown) {
         console.error(error);
     }
 }
